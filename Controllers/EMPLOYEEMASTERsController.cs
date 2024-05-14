4a463de7e891c97f937d72239aa6359abbe3f4e1
Swapnil Santosh Muluk <swapnilmuluk6@gmail.com>
2024-05-14 22:48:23 +0530

    Add project files.

---
+++ Controllers/EMPLOYEEMASTERsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Test1DBFirst.Models;
+
+namespace Test1DBFirst.Controllers
+{
+    public class EMPLOYEEMASTERsController : Controller
+    {
+        private CRUDEFEntities2 db = new CRUDEFEntities2();
+
+        // GET: EMPLOYEEMASTERs
+        public ActionResult Index()
+        {
+            return View(db.EMPLOYEEMASTERs.ToList());
+        }
+
+        // GET: EMPLOYEEMASTERs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYEEMASTER eMPLOYEEMASTER = db.EMPLOYEEMASTERs.Find(id);
+            if (eMPLOYEEMASTER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYEEMASTER);
+        }
+
+        // GET: EMPLOYEEMASTERs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EMPLOYEEMASTERs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,EMPCODE,EMPNAME,DESIGNATION,SALARY")] EMPLOYEEMASTER eMPLOYEEMASTER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EMPLOYEEMASTERs.Add(eMPLOYEEMASTER);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eMPLOYEEMASTER);
+        }
+
+        // GET: EMPLOYEEMASTERs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYEEMASTER eMPLOYEEMASTER = db.EMPLOYEEMASTERs.Find(id);
+            if (eMPLOYEEMASTER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYEEMASTER);
+        }
+
+        // POST: EMPLOYEEMASTERs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,EMPCODE,EMPNAME,DESIGNATION,SALARY")] EMPLOYEEMASTER eMPLOYEEMASTER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eMPLOYEEMASTER).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eMPLOYEEMASTER);
+        }
+
+        // GET: EMPLOYEEMASTERs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYEEMASTER eMPLOYEEMASTER = db.EMPLOYEEMASTERs.Find(id);
+            if (eMPLOYEEMASTER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYEEMASTER);
+        }
+
+        // POST: EMPLOYEEMASTERs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EMPLOYEEMASTER eMPLOYEEMASTER = db.EMPLOYEEMASTERs.Find(id);
+            db.EMPLOYEEMASTERs.Remove(eMPLOYEEMASTER);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Test1DBFirst.MvcApplication" Language="C#" %>
